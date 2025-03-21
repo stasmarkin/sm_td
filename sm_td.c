94ce27fde1a3671b63c1b06e9558d8ccac2793f5
Stanislav Markin <stasmarkin@gmail.com>
2025-03-21 17:21:31 -0300

    vibe coding tests

--- sm_td.c
+++ sm_td.c
@@ -9,6 +9,17 @@
 
 uint16_t keymaps[32][MATRIX_ROWS][MATRIX_COLS] = {0};
 
+// Implementation of on_smtd_action function
 smtd_resolution on_smtd_action(uint16_t keycode, smtd_action action, uint8_t sequence_len) {
     return SMTD_RESOLUTION_UNHANDLED;
+}
+
+// Implementation of get_smtd_timeout function
+uint32_t get_smtd_timeout(uint16_t keycode, smtd_timeout timeout) {
+    return get_smtd_timeout_default(timeout);
+}
+
+// Implementation of smtd_feature_enabled function
+bool smtd_feature_enabled(uint16_t keycode, smtd_feature feature) {
+    return smtd_feature_enabled_default(keycode, feature);
 }
